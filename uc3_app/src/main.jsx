9de721ea07a529c6f1dd8937faad4e496649d61d
Gabriela <gabriela.psantos48@senacsp.edu.br>
2025-05-01 15:30:04 -0300

    salvar

--- uc3_app/src/main.jsx
+++ uc3_app/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import Campeonato from './assets/Componentes/Campeonato'
+import App from './assets/Componentes/Campeonato'
 
 
 createRoot(document.getElementById('root')).render(
