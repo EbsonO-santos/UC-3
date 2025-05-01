2d5d4e3a0eaccf2444cc120ad8f500ca9b56e35d
Amani <amanivital23@gmail.com>
2025-05-01 16:37:56 -0300

    faixa

--- uc3_app/src/assets/Componentes/Promocao.jsx
+++ uc3_app/src/assets/Componentes/Promocao.jsx
@@ -1,11 +1,10 @@
 import Styles from './Promocao.module.css'
-import faixa from '../img/FAIXA.png'
 
 function Promocao() {
     return (
         <>
             <section className={Styles.Promocao}></section>
-            <img src={faixa} alt="" />
+            <section className={Styles.faixa}></section>
         </>
     );
 }
