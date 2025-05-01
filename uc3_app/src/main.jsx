3747d04c05c9128531a2d4841b54ca23e309cc50
Amani <amanivital23@gmail.com>
2025-05-01 15:24:49 -0300

    salvando

--- uc3_app/src/main.jsx
+++ uc3_app/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Promocao from './assets/Componentes/Promocao'
+import App from './assets/Componentes/Promocao'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
