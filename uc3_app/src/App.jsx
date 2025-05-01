408d905a3e21a4854a1fcf51e067dd99dda3ebe7
Amani <amanivital23@gmail.com>
2025-05-01 17:17:55 -0300

    salvar

--- uc3_app/src/App.jsx
+++ uc3_app/src/App.jsx
@@ -1,12 +1,8 @@
 import './App.css'
 import Header from './assets/Componentes/Header'
 import Conteudo from './assets/Componentes/Conteudo'
-<<<<<<< HEAD
 import Promocao from './assets/Componentes/Promocao'
 import Loja from './assets/Componentes/Loja'
-=======
-import Campeonato from './assets/Componentes/Campeonato'
->>>>>>> 6be4967cb055c67106ef6ff7dd73be51c89ddb57
 
 function App() {
 
@@ -14,12 +10,8 @@ function App() {
     <main className={'fundo'}>
       <Header />
       <Conteudo />
-<<<<<<< HEAD
       <Promocao />
       <Loja />
-=======
-      <Campeonato />
->>>>>>> 6be4967cb055c67106ef6ff7dd73be51c89ddb57
     </main>
   )
 }
